d97c08ecb9fe8352bd9dc67c9625a1ec9ee66781
henriquecscode <henriquecostasousa.hcs@gmail.com>
2023-04-25 15:45:34 +0100

    wikidata exception safety

--- pipeline/wikidata.py
+++ pipeline/wikidata.py
@@ -1,7 +1,8 @@
 import mkwikidata
 import pandas as pd
 
-def get_awards(imdb_id): 
+
+def get_awards(imdb_id):
     awards_query = f"""
         SELECT ?awardLabel ?dateLabel ?workLabel ?workIMDbId
         WHERE 
@@ -19,10 +20,13 @@ def get_awards(imdb_id):
             SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }} # Helps get the label in your language, if not, then en language      
         }}
         """
-
-    awards_result = mkwikidata.run_query(awards_query, params={ })
+    try:
+        awards_result = mkwikidata.run_query(awards_query, params={})
+    except Exception as e:
+        return []
 
     return [{"name_award": x["awardLabel"]["value"], "received_on": x["dateLabel"]["value"] if "dateLabel" in x else None, "imdb_id":  x["workIMDbId"]["value"] if "workLabel" in x else None} for x in awards_result["results"]["bindings"]]
+
 
 def actor_info(imdb_id):
     # Add country info
@@ -35,18 +39,25 @@ def actor_info(imdb_id):
             SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }} # Helps get the label in your language, if not, then en language      
         }}
         """
-    query_result = mkwikidata.run_query(query, params={ })
+    try:
+        query_result = mkwikidata.run_query(query, params={})
+    except Exception as e:
+        return {}
 
-    data = [{"name" : x["pLabel"]["value"], "country" : x["countryLabel"]["value"]} for x in query_result["results"]["bindings"]]
+    data = [{"name": x["pLabel"]["value"], "country": x["countryLabel"]["value"]}
+            for x in query_result["results"]["bindings"]]
 
+    if len(data) == 0:
+        data = [{}]
     # Add awards info
     data[0]["awards"] = get_awards(imdb_id)
-    
+
     df = pd.DataFrame(data)
-    print(df)
+    # print(df)
+    return data[0]
 
 
-#title P1476, summary P921 (subject?), duration P2047, release P577
+# title P1476, summary P921 (subject?), duration P2047, release P577
 def film_info(imdb_id):
     query = f"""
         SELECT ?filmLabel ?seriesLabel ?nrSerieLabel ?countryLabel ?genreLabel ?locationLabel ?producerCompanyLabel
@@ -66,22 +77,41 @@ def film_info(imdb_id):
             SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }} # Helps get the label in your language, if not, then en language      
         }}
         """
-    
-    query_result = mkwikidata.run_query(query, params={ })
+    try:
+        query_result = mkwikidata.run_query(query, params={})
+    except Exception as e:
+        return {}
 
+<<<<<<< HEAD
     data = [{"film" : x["filmLabel"]["value"], "series" : x["seriesLabel"]["value"] if "seriesLabel" in x else None, "seriesNr" : x["nrSerieLabel"]["value"]  if "nrSerieLabel" in x else None, "country" :  x["countryLabel"]["value"]} for x in query_result["results"]["bindings"]]
     data[0]["genres"] = list(set([x["genreLabel"]["value"] for x in query_result["results"]["bindings"]]))
     data[0]["producer company"] = list(set([x["producerCompanyLabel"]["value"] for x in query_result["results"]["bindings"]]))
     data[0]["location"] = list(set([x["locationLabel"]["value"] for x in query_result["results"]["bindings"]]))
+=======
+    data = [{"film": x["filmLabel"]["value"], "series": x["seriesLabel"]["value"],
+             "country":  x["countryLabel"]["value"]} for x in query_result["results"]["bindings"]]
+    if len(data) == 0:
+        data = [{}]
+    data[0]["genres"] = list(set([x["genreLabel"]["value"]
+                             for x in query_result["results"]["bindings"]]))
+    data[0]["producer company"] = list(set(
+        [x["producerCompanyLabel"]["value"] for x in query_result["results"]["bindings"]]))
+    data[0]["location"] = list(
+        set([x["locationLabel"]["value"] for x in query_result["results"]["bindings"]]))
+>>>>>>> 5e2ed0a... wikidata exception safety
     data[0]["awards"] = get_awards(imdb_id)
 
     df = pd.DataFrame(data)
-    
-    print(df.iloc[:1])
+
+    # print(df.iloc[:1])
+    return data[0]
 
 
-# Jennifer Lawrence Info
-actor_info("nm2225369")
+if __name__ == "__main__":
+    # Jennifer Lawrence Info
+    info = actor_info("nm2225369")
+    print(info)
 
-# Avatar Info
-film_info("tt0499549")
+    # Avatar Info
+    info = film_info("tt0499549")
+    print(info)
