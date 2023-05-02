3621f6a117f8bcaaaea89f686d75bb6a2f23c637
Catarina Pires <catarina.o.pires@hotmail.com>
2023-05-02 13:04:28 +0100

    Add movies page

--- frontend/src/views/movies/Movies.js
+++ frontend/src/views/movies/Movies.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import FilmCard from "../../components/cards/FilmCard";
+import SearchForm from "../../components/layout/SearchForm";
 
 function Movies() {
+  const movies = [{ name: "Avatar", link: "/movies/1" }, { name: "Avatar: Way of Water" }];
+
   return (
     <div>
-      <h1>Movies</h1>
-      <p>Movies page</p>
-      <Link to="/movies/1">Movie 1</Link>
+      <h1 className="text-center m-5">Movies</h1>
+      <Container>
+        <div className="d-flex bd-highlight">
+          <div className="p-2 flex-shrink-1 bd-highlight">
+            <h5>Search</h5>
+            <SearchForm />
+          </div>
+          <div className="p-2 w-100 bd-highlight">
+            <h5>Results</h5>
+            <div className="d-flex flex-column bd-highlight mb-3">
+              {movies.map(m => <FilmCard film={m} />)}
+            </div>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 }
