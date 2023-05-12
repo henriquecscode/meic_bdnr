a8fd8f0abf2c2c49cdd38ff2d0c0103c7b11f9f6
Patricia Oliveira <up201905427@fe.up.pt>
2023-05-12 11:57:33 +0100

    feat: profile interaction from user information

--- frontend/src/components/layout/lists/InteractionsList.js
+++ frontend/src/components/layout/lists/InteractionsList.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 
-function InteractionsList({ name, interactions }) {
+function InteractionsList({ username, name, interactions }) {
+  console.log(interactions);
   return (
     <div>
       <h4 className="mb-3">{name}</h4>
@@ -13,9 +14,26 @@ function InteractionsList({ name, interactions }) {
           {interactions.map((interaction) => (
             <Card key={interaction.id} className="card-item w-100">
               <Card.Body>
-                <Card.Title>{interaction.movie.title}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">{`Vote: ${interaction.vote}`}</Card.Subtitle>
-                <Card.Text>{interaction.comment}</Card.Text>
+                <Card.Title>
+                  <Card.Link
+                    href={`/movies/${interaction.movie.tid}?username=${username}`}
+                    target="_blank"
+                    style={{ color: "inherit", textDecoration: "inherit" }}
+                  >
+                    {interaction.movie.name}
+                  </Card.Link>
+                </Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{`Vote: ${
+                  interaction.vote ?? "--"
+                }`}</Card.Subtitle>
+
+                <Card.Text className="line-ellipsis">
+                  {interaction.comment ?? <i>No Comment...</i>}
+                </Card.Text>
+
+                <span className="blockquote-footer ps-2">
+                  <cite title="date">{interaction.date.split("T")[0]}</cite>
+                </span>
               </Card.Body>
             </Card>
           ))}
--- frontend/src/views/profile/Profile.js
+++ frontend/src/views/profile/Profile.js
@@ -34,43 +34,28 @@ function Profile({ username }) {
     },
   ];
 
-  // const movies = [
+  // const interactions = [
   //   {
   //     id: 1,
-  //     image: "movie1.jpg",
-  //     title: "Movie 1",
-  //     genre: "Action",
+  //     movie: {
+  //       id: 1,
+  //       title: "Movie 1",
+  //     },
+  //     vote: 1,
+  //     comment:
+  //       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
   //   },
   //   {
   //     id: 2,
-  //     image: "movie2.jpg",
-  //     title: "Movie 2",
-  //     genre: "Action",
+  //     movie: {
+  //       id: 2,
+  //       title: "Movie 2",
+  //     },
+  //     vote: 1,
+  //     comment:
+  //       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
   //   },
   // ];
-
-  const interactions = [
-    {
-      id: 1,
-      movie: {
-        id: 1,
-        title: "Movie 1",
-      },
-      vote: 1,
-      comment:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-    },
-    {
-      id: 2,
-      movie: {
-        id: 2,
-        title: "Movie 2",
-      },
-      vote: 1,
-      comment:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-    },
-  ];
 
   const series = [
     {
@@ -90,7 +75,7 @@ function Profile({ username }) {
   const [user, setUser] = useState({});
   // const [friends, setFriends] = useState([]);
   const [movies, setMovies] = useState([]);
-  // const [interactions, setInteractions] = useState([]);
+  const [interactions, setInteractions] = useState([]);
   // const [series, setSeries] = useState([]);
 
   useEffect(() => {
@@ -103,11 +88,21 @@ function Profile({ username }) {
         console.log(data);
         // setFriends(data.friends);
         setMovies(data.toWatch);
+        setInteractions(
+          data.films.map((film, index) => {
+            return {
+              id: index,
+              movie: film.title,
+              ...film.watched,
+            };
+          })
+        );
       },
       (error) => {
         setUser({});
         // setFriends([]);
         setMovies([]);
+        setInteractions([]);
         console.log(error);
       }
     );
@@ -138,6 +133,7 @@ function Profile({ username }) {
               />
               <br />
               <InteractionsList
+                username={username}
                 name={"Interactions"}
                 interactions={interactions}
               />
