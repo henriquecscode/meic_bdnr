d950d8aa5e1e5179d5bffcba00320f86b18101cc
henriquecscode <henriquecostasousa.hcs@gmail.com>
2023-05-14 16:05:51 +0100

    password consistency

--- backend/src/main/java/database/CreateSocialNetwork.java
+++ backend/src/main/java/database/CreateSocialNetwork.java
@@ -27,9 +27,9 @@ public class CreateSocialNetwork {
     // Using document API
     static String dbname = "app_sample";
     static String user = "root";
-    static String password = "root";
+    static String password = "rootpwd";
     static String dbUser = "root";
-    static String dbPassword = "root";
+    static String dbPassword = "rootpwd";
     static OrientGraphFactory factory;
     static OrientGraph graph;
     static String rootDir = "../";
